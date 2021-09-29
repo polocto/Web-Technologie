1a36c46c5d09cb59d27bf3b06b553ef077c58144
polocto <paul.sade@live.fr>
2021-09-29 15:12:48 +0200

    Supp html

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 // Import a module
 const http = require('http')
 
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
 
 // Import Node url module
 const url = require('url')
