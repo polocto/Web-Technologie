5481ffd5b5a451b181e2b011beae8d450564947a
mathisCAMARD <matt.camard@gmail.com>
2021-09-29 14:12:32 +0200

    Create Http server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Hello nodejs");
+
+// Import a module
+const http = require('http')
+
+// Declare an http server
+http.createServer(function (req, res) {
+
+  // Write a response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Write a response content
+  res.end('Hello World\n');
+
+// Start the server
+}).listen(8080)
