7ca5277ab1b824eb38f05ec0e28eec3c293ec295
polocto <paul.sade@live.fr>
2021-09-29 14:30:20 +0200

    Sending back HTML

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // Import a module
 const http = require('http')
 
-// Declare an http server
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
 
 const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Firefox\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(content);
+  res.end();
 }
 
+// Declare an http server
 const server = http.createServer(serverHandle);
 server.listen(8080)
