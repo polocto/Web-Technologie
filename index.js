dc45f064739033136737eb68b0f7ca3f13daa073
mathisCAMARD <matt.camard@gmail.com>
2021-09-29 15:31:23 +0200

    Organize the source code module

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-// Import a module
-const http = require('http')
-
-
-// Import Node url module
-const url = require('url')
-const qs = require('querystring')
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.end();
-}
-
-// Declare an http server
-const server = http.createServer(serverHandle);
-server.listen(8080)
+const http = require("http");
+const handles = require("./handle");
+const server = http.createServer(handles.serverHandle);
+server.listen(8080);
