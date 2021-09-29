556de72dd60af1f42f66a76ccbc4d231c33a079d
polocto <paul.sade@live.fr>
2021-09-29 14:39:55 +0200

    query

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ const content = '<!DOCTYPE html>' +
 
 // Import Node url module
 const url = require('url')
+const qs = require('querystring')
 
 const serverHandle = function (req, res) {
-  // Retrieve and print the current path
-  const path = url.parse(req.url).pathname;
-  console.log(path);
+  // Retrieve and print the queryParams
+  const queryParams = qs.parse(url.parse(req.url).query);
+  console.log(queryParams);
 
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(content);
