fdbf8cdea133904575e9e9bd9ca06035b771a534
polocto <paul.sade@live.fr>
2021-09-29 14:54:01 +0200

    Use information from url in web page

--- index.js
+++ index.js
@@ -18,12 +18,18 @@ const url = require('url')
 const qs = require('querystring')
 
 const serverHandle = function (req, res) {
-  // Retrieve and print the queryParams
-  const queryParams = qs.parse(url.parse(req.url).query);
-  console.log(queryParams);
+  const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
+  
   res.end();
 }
 
