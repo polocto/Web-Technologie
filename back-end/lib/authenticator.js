48035af3b3cc69efe0e3dbcbf705b6552179f7f1
Paul SADE <paul.sade@live.fr>
2021-12-13 10:27:40 +0100

    Add ';' in the backend to make the code more readable

--- back-end/lib/authenticator.js
+++ back-end/lib/authenticator.js
@@ -1,16 +1,16 @@
 
-const jwksClient = require('jwks-rsa')
-const jwt = require('jsonwebtoken')
+const jwksClient = require('jwks-rsa');
+const jwt = require('jsonwebtoken');
 
 const fetchKeyFromOpenIDServer = async (jwks_uri, token) => {
   const header = JSON.parse( Buffer.from(
       token.split('.')[0], 'base64'
     ).toString('utf-8')
-  )
+  );
   const {publicKey, rsaPublicKey} = await jwksClient({
     jwksUri: jwks_uri
-  }).getSigningKey(header.kid)
-  return publicKey || rsaPublicKey
+  }).getSigningKey(header.kid);
+  return publicKey || rsaPublicKey;
 }
 
 module.exports = ({jwks_uri, test_payload_email} = {}) => {
@@ -26,32 +26,32 @@ module.exports = ({jwks_uri, test_payload_email} = {}) => {
         email: test_payload_email,
         email_verified: true
       }
-      next()
-    }
+      next();
+    };
     
   }
   if(!jwks_uri){
-    throw Error('Invalid Settings: jwks_uri is required')
+    throw Error('Invalid Settings: jwks_uri is required');
   }
   return async (req, res, next) => {
     if(! req.headers['authorization'] ){
-      res.status(401).send('Missing Access Token')
-      return
+      res.status(401).send('Missing Access Token');
+      return;
     }
-    const header = req.headers['authorization']
-    const [type, access_token] = header.split(' ')
+    const header = req.headers['authorization'];
+    const [type, access_token] = header.split(' ');
     if(type !== 'Bearer'){
-      res.status(401).send('Authorization Not Bearer')
-      return
+      res.status(401).send('Authorization Not Bearer');
+      return;
     }
-    const key = await fetchKeyFromOpenIDServer(jwks_uri, access_token)
+    const key = await fetchKeyFromOpenIDServer(jwks_uri, access_token);
     // Validate the payload
     try{
-      const payload = jwt.verify(access_token, key)
-      req.user = payload
-      next()
+      const payload = jwt.verify(access_token, key);
+      req.user = payload;
+      next();
     }catch(err){
-      res.status(401).send('Invalid Access Token')
+      res.status(401).send('Invalid Access Token');
     }
   }
 }
