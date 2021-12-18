c8b517decb1ac86e6867e16be3b3c8e739a06f5e
Paul SADE <paul.sade@live.fr>
2021-12-18 12:12:52 +0100

    Channels functions

--- back-end/lib/db/channels.js
+++ back-end/lib/db/channels.js
@@ -7,60 +7,137 @@ const StatusError = require('../error');
 const users = require('./users');
 
 module.exports = {
-    create: async (channel,idUser) => {
-      if(!channel.name) throw StatusError(400,'Invalid channel');   
+    create: async (metadata,idUser) => {
+      if(!metadata.name) throw StatusError(400,'Invalid channel');   
       const id = uuid();
       const presentTime= [{
         arrivalTime: microtime.now()
       }];
 
-      channel.users = Promise.all(channel.users.map(async (idU) => {
-        const user = await users.get(idU);
+      let channel = {...metadata};
+      
+      if(!channel.users.some(elem => elem.match(idUser))) channel.users.push(idUser);
+      
+      channel.users = await Promise.all(channel.users.map(async (idU) => {
+        let user = await users.get(idU);
         user.channels.push(id);
         delete user.id;
         await db.put(`users:${idU}`, JSON.stringify(user));
-        return merge({id: idU}, {presentTime: {...presentTime}})
-      },this));
+        return merge({id: idU}, {presentTime: [...presentTime]})
+      }));
 
-      channel = merge(channel,{admin: [idUser] })
+      channel = merge(channel,
+      {
+        admin: [idUser],
+        pinned: [],
+        shareFile: [],
+        shareImage: [],
+        shareLinks: [] 
+      })
       await db.put(`channels:${id}`, JSON.stringify(channel));
-      return await this.users.get(idUser);
+      return merge({id: id}, channel);
     },
 
     get: async (id) => {
       if(!id) throw StatusError(400,'Invalid id');
-      const data = await db.get(`channels:${id}`);
-      const channel = JSON.parse(data);
-      return merge({id: id}, channel);
+      try
+      {
+        const data = await db.get(`channels:${id}`);
+        const channel = JSON.parse(data);
+        return merge({id: id}, channel);
+      }
+      catch(err)
+      {
+        throw new StatusError(404, `Channel's ${id} doesn't exist`);
+      }
     },
 
-    list: async () => {
-      return new Promise( (resolve, reject) => {
-        const channels = [];
-        db.createReadStream({
-          gt: "channels:",
-          lte: "channels" + String.fromCharCode(":".charCodeAt(0) + 1),
-        }).on( 'data', ({key, value}) => {
-          channel = JSON.parse(value);
-          channel.id = key.split(':')[1];
-          channels.push(channel);
-        }).on( 'error', (err) => {
-          reject(err);
-        }).on( 'end', () => {
-          resolve(channels);
+    list: async function (channels) {
+      if(!channels)
+      {
+        return new Promise( (resolve, reject) => {
+          const channels = [];
+          db.createReadStream({
+            gt: "channels:",
+            lte: "channels" + String.fromCharCode(":".charCodeAt(0) + 1),
+          }).on( 'data', ({key, value}) => {
+            channel = JSON.parse(value);
+            channel.id = key.split(':')[1];
+            channels.push(channel);
+          }).on( 'error', (err) => {
+            reject(err);
+          }).on( 'end', () => {
+            resolve(channels);
+          });
         });
+      }
+      else
+      {
+        return await Promise.all(channels.map(async (id) =>{
+          let channel = await this.get(id);
+          return channel;
+        },this));
+      }
+    },
+
+    update: async function (channel){
+      const original = await this.get(channel.id);
+      const originalKeys = Object.keys(original);
+      const channelKeys = Object.keys(channel);
+      const time = microtime.now();
+
+      if(originalKeys.length != channelKeys.length) throw new StatusError(403, `wrong number keys`);
+      //update access to channel for all existing users
+      await original.users.map( async user => {
+        const userToModify = await users.get(user.id);
+        if(channel.users.some(u => u.id.match(user.id)))
+        {
+          const keys = Object.keys(user.presentTime[user.presentTime.length-1]);
+          if(keys.some(elem => elem.match(/departureTime/)))
+          {
+            user.presentTime.push({arrivalTime: time});
+            userToModify.channels.push(channel.id);
+          }
+        }
+        else
+        {
+          const keys = Object.keys(user.presentTime[user.presentTime.length-1]);
+          if(!keys.some(elem => elem.match(/departureTime/)))
+          {
+            user.presentTime[user.presentTime.length-1].departureTime = time;
+            userToModify.channels.splice(userToModify.channels.indexOf(channel.id),1);
+          }
+        }
+        const result = await users.update(userToModify);
+        if(!result.id.match(user.id)) throw new StatusError(409,"Unknow Error return id does not match");
       });
+
+      channel.users.map( u => {
+        if(!original.users.some(user => user.id.match(u.id)))
+        {
+          original.users.push({
+            id: u.id,
+            presentTime: [{arrivalTime: time}]
+          });
+        }
+      });
+
+      delete original.id;
+      await db.put(`channels:${channel.id}`, JSON.stringify(original));
+      return channel;
     },
 
-    update: (id, channel) => {
-      const original = store.channels[id];
-      if(!original) throw StatusError(404,'Unregistered channel id');
-      store.channels[id] = merge(original, channel);
-    },
-
-    delete: (id, channel) => {
-      const original = store.channels[id];
-      if(!original) throw StatusError(404,'Unregistered channel id');
-      delete store.channels[id];
+    delete: async function (channel, admin) {
+      if(channel.users.length != 0 && admin && !channel.admin.some(id => id.match(admin))) throw new StatusError(403,)
+      //delete channel from all users
+      channel.users.map(async userId => {
+        const user = await users.get(userId);
+        user.channels.splice(user.channels.indexOf(channel.id),1);
+        await users.update(user);
+      })
+      channel.users = [];
+      channel = await this.update(channel);
+      const regex = new RegExp("channels:" + channel.id + ".*");
+      await db.del(regex)
     }
 }
--- back-end/lib/route/channels.js
+++ back-end/lib/route/channels.js
@@ -1,32 +1,180 @@
 const db = require('../db/db');
 const express = require('express');
 const messages = require('./messages');
+const StatusError = require('../error');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const channels = await db.channels.list();
-    res.json(channels);
+    try{
+        const user = await db.users.get(req.body.id);
+        const channels = await db.channels.list(user.channels);
+        channels.map(channel => {
+          delete channel.users;
+          delete channel.admin;
+          delete channel.shareFile;
+          delete channel.shareImage;
+          delete channel.shareLinks;
+          delete channel.pinned;
+          return channel;
+        })
+        res.status(200).json(channels);
+    }
+    catch(err){
+        if (err instanceof StatusError)
+        {
+            res.status(err.status).send(err.message);
+        }
+        else
+        {
+            console.error(err);
+            res.status(520).send(err.message);
+        }
+    }
 });
 
 router.post('/', async (req, res) => {
     try{
-        const [metadata, userId] = req.body;
-        const channel = await db.channels.create(metadata,userId);
-        res.status(201).json(channel);
+        const [metadata, user] = req.body;
+        if(metadata.users.length <2)
+        {
+            throw new StatusError (403,"You should select more users");
+        }
+        else
+        {
+            const channel = await db.channels.create(metadata,user.id);
+            const newUser = await db.users.get(user.id);
+            if(!channel.users.some(elem => elem.id.match(newUser.id))) throw new StatusError(409,"User haven't be add well to the channel");
+            if(!newUser.channels.some(elem => elem.match(channel.id))) throw new StatusError(409,"Channel haven't be add well to the user");
+            res.status(201).json(newUser);
+        }
     }
     catch(err){
-        res.status(err.status).send(err.messages);
+        if (err instanceof StatusError)
+        {
+            res.status(err.status).send(err.message);
+        }
+        else
+        {
+            console.error(err);
+            res.status(520).send(err.message);
+        }
     }
+});
+
+router.put('/', async (req,res)=>{
+    
 });
 
 router.get('/:id', async (req, res) => {
-    const channel = await db.channels.get(req.params.id);
-    res.json(channel);
+    try{
+        const user = await db.users.get(req.body.id);
+        if(!user.channels.some(elem => elem.match(req.params.id)))
+        {
+            throw new StatusError(404,"This user have no right to access this channel");
+        }
+        const channel = await db.channels.get(req.params.id);
+        channel.users = await db.users.list(channel.users.map(user => {
+            
+            if(user.presentTime.length<1) throw new Error("list channel -> user.presentTime.length < 1");
+            const presentTime = user.presentTime[user.presentTime.length - 1]
+            const keys = Object.keys(presentTime);
+            if(!keys.some(elem => elem.match(/departureTime/)))
+              return user.id;
+            else
+                return null;
+    
+        }).filter(user => user!=null));
+
+
+        channel.admin = channel.admin.some(elem => elem.match(req.body.id));
+
+        res.status(200).json(channel);
+    }
+    catch(err){
+        if (err instanceof StatusError)
+        {
+            res.status(err.status).send(err.message);
+        }
+        else
+        {
+            console.error(err);
+            res.status(520).send(err.message);
+        }
+    }
 });
 
 router.put('/:id', async (req, res) => {
-    const channel = await db.channels.update(req.body);
-    res.json(channel);
+    try{
+        let [metadata, user] = req.body;
+        user = await db.users.get(user.id);
+        if(!user.channels.some(elem=>elem.match(req.params.id))) throw new StatusError(401,"You don't have the authorization to access to this channel");
+        const channel = await db.channels.get(req.params.id);
+        
+        const keys = Object.keys(metadata);
+
+        keys.map(key =>{
+            if(!key.match(/share/) && !key.match(/admin/) && Object.keys(channel).some(k => k.match(key)))
+            {
+                if(!key.match(/users/) || channel.admin.some(id => id.match(user.id)))
+                    channel[key] = metadata[key];
+                else
+                    throw new StatusError(402, "You can't modify the users of the channel");
+            }
+        });
+        
+        res.status(200).json(channel);
+    }
+    catch(err){
+        if (err instanceof StatusError)
+        {
+            res.status(err.status).send(err.message);
+        }
+        else
+        {
+            console.error(err);
+            res.status(520).send(err.message);
+        }
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const user = await db.users.get(req.body.id);
+
+        const channel = await db.channels.get(req.params.id);
+        channel.users = await db.users.list(channel.users.map(user => {
+            
+            if(user.presentTime.length<1) throw new Error("list channel -> user.presentTime.length < 1");
+            const presentTime = user.presentTime[user.presentTime.length - 1]
+            const keys = Object.keys(presentTime);
+            if(!keys.some(elem => elem.match(/departureTime/)))
+              return user.id;
+            else
+                return null;
+    
+        }).filter(user => user!=null));
+        if(!channel.users.some(u => u.id.match(user.id))) throw new StatusError(409, "User is not in the channel");
+
+        const index = channel.users.findIndex(item => item.id.match(user.id));
+        channel.users.splice(index,1);
+        user.channels.splice(user.channels.indexOf(channel.id),1);
+        db.channels.update(channel)
+        .then(async (value) => {
+            if(!value.users.length) db.channels.delete(value);
+        })
+        res.status(200).json(user);
+    }
+    catch(err){
+        if (err instanceof StatusError)
+        {
+            res.status(err.status).send(err.message);
+        }
+        else
+        {
+            console.error(err);
+            res.status(520).send(err.message);
+        }
+    }
 });
 
 router.use('/:id*', messages);
